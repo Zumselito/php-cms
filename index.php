5c5c9567cddcbc2b7ccd9f43fd612b996d473b62
Zumsel <zumselito@gmail.com>
2024-12-15 14:08:45 +0000

    navbar update

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-
+include('./includes/config.inc.php');
 include('./includes/database.inc.php');
+include('./includes/functions.inc.php');
+include('./includes/header.inc.php');
+?>
 
-echo 'Hello World!' ?>
+index.php content
+
+<?php
+include('./includes/footer.inc.php');
+?>
---
+++ js/tailwind.config.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'tomato': {
+          '50': '#fff3f1',
+          '100': '#ffe4df',
+          '200': '#ffcec5',
+          '300': '#ffac9d',
+          '400': '#ff7c64',
+          DEFAULT: '#ff6347',
+          '500': '#ff6347',
+          '600': '#ed3615',
+          '700': '#c8290d',
+          '800': '#a5260f',
+          '900': '#882614',
+          '950': '#4b0f04',
+        }
+      }
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    }
+  }
+}
